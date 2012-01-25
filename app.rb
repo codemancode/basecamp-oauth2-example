9c84abbf56eb465458ec8e659da1e9967b085831
Matthew McFarling <matthew.mcfarling@gmail.com>
2012-01-24 22:14:11 -0500

    moved client id, client secret and callback url into config/basecamp.yml file

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
 module Launchpad
   class Basecamp < Sinatra::Base
 
-    set :client_id, 'YOUR CLIENT ID'
-    set :client_secret, 'YOUR CLIENT SECRET'
-    set :callback_url, 'YOUR CALLBACK URL'
+    YAML::load(File.open('config/basecamp.yml'))['config'].each do |k,v|
+      set k, v
+    end
 
     set :views, 'views'
     set :raise_errors, Proc.new { false }
@@ -17,12 +17,17 @@ module Launchpad
       session[:oauth] ||= {}
       
       @client = client
-    
+  
       if !session[:oauth][:access_token].nil?
-        @token = OAuth2::AccessToken.new(@client, session[:oauth][:access_token])
-        if @token.expired?
-          @token = @token.refresh!(:refresh_token => @token.refresh_token)
-        end
+        @token = OAuth2::AccessToken.new(@client, 
+                                         session[:oauth][:access_token], 
+                                         {:refresh_token => session[:oauth][:refresh_token],
+                                         :expires_at => session[:oauth][:expires_at],
+                                         :expires_in => session[:oauth][:expires_in]})
+        
+         if @token.expired?
+           @token = @token.refresh!(:refresh_token => @token.refresh_token, :type => 'refresh')
+         end
       end
     end
 
@@ -42,6 +47,9 @@ module Launchpad
       if params[:code] != nil
         @token = client.auth_code.get_token(params[:code], :redirect_uri => redirect_uri, :type => 'web_server')
         session[:oauth][:access_token] = @token.token
+        session[:oauth][:refresh_token] = @token.refresh_token
+        session[:oauth][:expires_at] = @token.expires_at
+        session[:oauth][:expires_in] = @token.expires_in
         redirect '/'
       else
         raise "General Error"
